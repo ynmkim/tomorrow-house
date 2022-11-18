1a7bed45fea40680b6f52c6a95546ffa26fa2170
김윤미 <younmeet@gmail.com>
2022-11-18 19:04:01 +0900

    tab-panel 이동 인터랙션 구현

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,10 +1,13 @@
 const productTab = document.querySelector('.product-tab')
 const productTabButtonList = productTab.querySelectorAll('button')
+const TOP_HEADER_PC = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
 
 // console.log(productTabButtonList)
 
 let currentActiveTab = productTab.querySelector('.is-active')
 
+// 탭 활성화
 function toggleActiveTab() {
   const tabItem = this.parentNode
 
@@ -18,4 +21,20 @@ function toggleActiveTab() {
 
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab)
+  button.addEventListener('click', scrollToTabPanel)
 })
+
+// 탭 패널로 이동
+function scrollToTabPanel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabPanel = document.querySelector(`#${tabPanelId}`)
+
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_PC : TOP_HEADER_MOBILE)
+
+  window.scrollBy({
+    top: scrollAmount,
+    behavior: 'smooth',
+  })
+}
